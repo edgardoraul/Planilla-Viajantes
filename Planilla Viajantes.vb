a8803c28ddc85a1066cc084ef259d159a69771c4
Edgard_Roul <edgardo_raul@hotmail.com>
2022-05-16 16:25:58 -0300

    Validación del Código Postal exitoso
    Los códigos postales son por lista.
    Una fórmula autocompleta el NIS. El resto es automático.
    Muestra la carpeta donde se guardó el archivo.
    Comenté la opción de Impresión.
    Protege el libro y hojas automáticamentte

--- Planilla Viajantes.vb
+++ Planilla Viajantes.vb
@@ -1,4 +1,13 @@
 Option Explicit
+
+' Definiendo unas variables globales a usar
+Dim cp As String
+Dim codigoNis As String
+Dim ruta As String
+Dim rutaViajante As String
+Dim viajante As String
+Dim cliente As String
+Dim fecha As String
 Sub Generar_Rotulo()
 ' ============================================================
 ' GENERA UN ROTULO Y PLANILLA PARA IMPRIMIR VENTA DE UN
@@ -6,12 +15,13 @@ Sub Generar_Rotulo()
 ' CREADO. SI NO, LAS CREA. TAMBIÉN GUARDA LOS ARCHIVOS CON
 ' NOMBRE DE LOS CLIENTES Y FECHA, CADA RESPECTIVA SUBCARPETA.
 ' ============================================================
-    ' Pretegiendo el libraco
-    ThisWorkbook.Protect
     
     ' Ocultando una hoja innecesaria
     If Worksheets("Lince").Visible = True Then
         Worksheets("Lince").Visible = False
+    End If
+    If Worksheets("Datos").Visible = True Then
+        Worksheets("Datos").Visible = False
     End If
     
     ' Protegiendo el resto de las hojas de cambios innecesarios
@@ -20,15 +30,8 @@ Sub Generar_Rotulo()
         Worksheets(cuenta).Protect
     Next
     
-    ' Definiendo variables a usar
-    Dim ruta As String
-    Dim rutaViajante As String
-    Dim viajante As String
-    Dim cliente As String
-    Dim fecha As String
-    Dim cp As String
-    Dim codigoNis As String
-    
+    ' Pretegiendo el libraco
+    ActiveWorkbook.Protect Password:="Rerda", Structure:=True, Windows:=True
     
     ' Asigando los valores de acuerdo a la planilla donde esté parado
     ruta = ThisWorkbook.Path & "\Ventas de Viajantes"
@@ -72,16 +75,6 @@ Sub Generar_Rotulo()
     ElseIf ActiveSheet.Range("X5").Value = "" Then
         MsgBox ("¿Y el código postal?")
         ActiveSheet.Range("X5").Select
-        
-    ' Controlando el código postal
-    ElseIf ActiveSheet.Name = "A Sucursal" And Sheets("A Sucursal").Range("X5").Value = "" Then
-        ' Validando si lo encuentra
-        If codigoNis = "" Then
-            ' Todo salió mal
-            Sheets("A Sucursal").Range("X5").Select
-            codigoNis = Sheets("Sucursales").Range("$F$4:$F$5000").Find(What:=cp, LookIn:=xlValues, LookAt:=xlPart).Offset(0, -4)
-            MsgBox ("El código postal " & cp & " no existe. Intentá con otro. ")
-        End If
     
     ' Controlando el DNI/CUIT
     ElseIf ActiveSheet.Range("X6").Value = "" Then
@@ -99,18 +92,48 @@ Sub Generar_Rotulo()
         ActiveSheet.Range("X8").Select
     
     Else
-    
+        ' Mostrar dónde se guardó el archivo
+        MsgBox ("Se guardó una copia PDF en " & rutaViajante & "\" & cliente & ". " & fecha & ".pdf")
+        
         ' Guardando el archivo
         ActiveSheet.ExportAsFixedFormat Type:=xlTypePDF, Filename:= _
             rutaViajante & "\" & cliente & ". " & fecha & ".pdf", Quality:=xlQualityStandard, _
             IncludeDocProperties:=True, IgnorePrintAreas:=False, OpenAfterPublish:= _
-            False
-        
-        ' Mostrar dónde se guardó el archivo
-        MsgBox ("Se guardó una copia PDF en " & rutaViajante & "\" & cliente & ". " & fecha & ".pdf")
+            True
         
         ' Imprimiendo el archivo
-        ActiveSheet.Application.Dialogs(xlDialogPrint).Show
+        'ActiveSheet.Application.Dialogs(xlDialogPrint).Show
+        
+        ' Muestra el archivo en carpeta para enviar por mail
+        Shell "explorer " & rutaViajante, vbNormalFocus
+    End If
+End Sub
+
+Private Sub Workbook_Activate()
+    ' Pretegiendo el libraco
+    ActiveWorkbook.Protect Password:="Rerda", Structure:=True, Windows:=True
+End Sub
+
+Private Sub Workbook_BeforeClose(Cancel As Boolean)
+    ' Pretegiendo el libraco
+    ActiveWorkbook.Protect Password:="Rerda", Structure:=True, Windows:=True
+End Sub
+
+Private Sub Workbook_Open()
+    ' Pretegiendo el libraco
+    ActiveWorkbook.Protect Password:="Rerda", Structure:=True, Windows:=True
+    
+    ' Ocultando una hoja innecesaria
+    If Worksheets("Lince").Visible = True Then
+        Worksheets("Lince").Visible = False
+    End If
+    If Worksheets("Datos").Visible = True Then
+        Worksheets("Datos").Visible = False
     End If
     
+    ' Protegiendo el resto de las hojas de cambios innecesarios
+    Dim cuenta As Integer
+    For cuenta = 1 To Worksheets.Count
+        Worksheets(cuenta).Protect
+    Next
 End Sub
